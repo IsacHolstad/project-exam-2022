9e64f72a130a98a757c85b66768fa8eea2ab32a0
IsacHolstad <wisac22@gmail.com>
2022-05-12 20:20:21 +0200

    final endpoint

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const id = params.get('id');
 //
 
 
-const myAPI = "http://isacblog.flywheelsites.com/wp-json";
+const myAPI = "http://isacblog.flywheelsites.com/wp-json/wp/v2/posts";
 //console.log(myAPI);
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + myAPI; 
