b5db5acd5e2cd24a911612bfddaf3e3b9af5ffb8
IsacHolstad <wisac22@gmail.com>
2022-05-12 20:32:49 +0200

    js page made

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get('id');
-//
-//
-//
-//
 
-
+//
+//
+//
+//
 const myAPI = "http://isacblog.flywheelsites.com/wp-json/wp/v2/posts";
 //console.log(myAPI);
 const proxy = "https://noroffcors.herokuapp.com/";
