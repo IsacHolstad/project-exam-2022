23bad91db2ab1b3719efb0c3a40802c8dbace614
IsacHolstad <wisac22@gmail.com>
2022-05-24 11:53:08 +0200

    added proxy to etail page

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,16 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get('id');
+
+////
+////
+////
+/////
+////
+/////
+////SPACING FOR NOW ONLY
+
+const ApiUrl = `https://isacblog.flywheelsites.com/wp-json/wp/v2/posts${id}`;
+console.log(ApiUrl)
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + ApiUrl;
