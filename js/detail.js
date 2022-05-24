3f950112bb886a8b77eb3efb5a3ffc7379fe513f
IsacHolstad <wisac22@gmail.com>
2022-05-24 11:58:25 +0200

    made post container and backbox

--- js/detail.js
+++ js/detail.js
@@ -14,3 +14,11 @@ const ApiUrl = `https://isacblog.flywheelsites.com/wp-json/wp/v2/posts${id}`;
 console.log(ApiUrl)
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + ApiUrl;
+console.log(corsFixUrl);
+
+///
+///
+///
+///
+///
+
