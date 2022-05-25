fe593093391813c5db321d315fd4a0535be41bb7
IsacHolstad <wisac22@gmail.com>
2022-05-25 18:53:53 +0200

    validation for name

--- js/contact.js
+++ js/contact.js
@@ -5,4 +5,6 @@ const isValidEmail = (email) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
-  console.log(isValidEmail)
+  
+  const nameInput = document.querySelector('input[name="name"]');
+  console.log(nameInput)
