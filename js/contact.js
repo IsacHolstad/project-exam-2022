48e89bf50bea2d87a91f0c6a511761eb79f3518a
IsacHolstad <wisac22@gmail.com>
2022-05-25 19:18:32 +0200

    gmail form

--- js/contact.js
+++ js/contact.js
@@ -5,6 +5,36 @@ const isValidEmail = (email) => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
-  
+  const form = document.querySelector('form')
+  const thankYou = document.querySelector('.thankyou')
+
   const nameInput = document.querySelector('input[name="name"]');
-  console.log(nameInput)
+  
+  let isFormValid = false;
+
+  const validateInputs = () => {
+    nameInput.classList.remove("invalid");
+    nameInput.nextElementSibling.classList.add("hidden")
+    isFormValid = true
+    if (!nameInput.value) {
+        nameInput.classList.add("invalid")
+        nameInput.nextElementSibling.classList.remove("hidden")
+        isFormValid = false;
+
+    }
+}
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      validateInputs();
+      if (isFormValid) {
+          form.remove();
+          thankYou.classList.remove("hidden")
+      }
+      
+
+  });
+  nameInput.addEventListener('input', () =>{
+      validateInputs();
+
+  })
